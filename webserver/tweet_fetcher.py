4de494c482881b910ce993eec3d41fc065f4bd4c
Mo Berra <moses.r.berra@gmail.com>
2019-01-20 13:57:03 -0600

    added window size to see if scrolling autoload will work with it on in heroku

--- webserver/tweet_fetcher.py
+++ webserver/tweet_fetcher.py
@@ -10,7 +10,8 @@ class TweetFetcher:
         # load twitter page using selenium
         options = webdriver.chrome.options.Options()
         options.add_argument('--no-sandbox')
-        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('window-size=1000x1000')
         options.set_headless(headless=True)
         if CHROMEDRIVER_PATH: # For heroku deploy only
             options.binary_location = CHROME_BIN
